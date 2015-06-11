7f8df9a1bcd5ccf874c014d11ccf3f0578a08ec6
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-06-11 18:49:36 +0900

    inspector, java: use correct package name and let constructor public
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- inspector/java/base/src/org/osrg/earthquake/HTTPEQHelper.java
+++ inspector/java/base/src/org/osrg/earthquake/HTTPEQHelper.java
@@ -1,17 +1,42 @@
 
+package org.osrg.earthquake;
+
 import org.osrg.earthquake.*;
 import java.util.*;
 import org.jboss.byteman.rule.*;
 import org.jboss.byteman.rule.helper.*;
 
-public class HTTPEQHelper extends EQHelper
+public class HTTPEQHelper extends Helper
 {
+    static HTTPInspector inspector;
+
     static {
-    	inspector = new org.osrg.earthquake.HTTPInspector();
+    	inspector = new HTTPInspector();
     };
 
     HTTPEQHelper(Rule rule) {
 	super(rule);
+    }
+
+    public static void activated() {
+	inspector.Initiation();
+	// System.out.println("BTM: initiation to orchestrator completed");
+    }
+
+    public static void deactivated() {
+        //stopThread();
+    }
+
+    public void eventFuncCall(String name) {
+	inspector.EventFuncCall(name);
+    }
+
+    public void eventFuncCall(String name, Map<String, Object> argMap) {
+	inspector.EventFuncCall(name, argMap);
+    }
+
+    public void stopInspection() {
+	inspector.StopInspection();
     }
 }
 
--- inspector/java/base/src/org/osrg/earthquake/PBEQHelper.java
+++ inspector/java/base/src/org/osrg/earthquake/PBEQHelper.java
@@ -1,16 +1,41 @@
 
+package org.osrg.earthquake;
+
 import org.osrg.earthquake.*;
 import java.util.*;
 import org.jboss.byteman.rule.*;
 import org.jboss.byteman.rule.helper.*;
 
-public class PBEQHelper extends EQHelper
+public class PBEQHelper extends Helper
 {
+    static PBInspector inspector;
+
     static {
-    	inspector = new org.osrg.earthquake.PBInspector();
+    	inspector = new PBInspector();
     };
 
-    PBEQHelper(Rule rule) {
+    public PBEQHelper(Rule rule) {
 	super(rule);
+    }
+
+    public static void activated() {
+	inspector.Initiation();
+	// System.out.println("BTM: initiation to orchestrator completed");
+    }
+
+    public static void deactivated() {
+        //stopThread();
+    }
+
+    public void eventFuncCall(String name) {
+	inspector.EventFuncCall(name);
+    }
+
+    public void eventFuncCall(String name, Map<String, Object> argMap) {
+	inspector.EventFuncCall(name, argMap);
+    }
+
+    public void stopInspection() {
+	inspector.StopInspection();
     }
 }
