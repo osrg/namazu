a94a8399b566bea5ba542da078a3ca447e626036
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-06-11 13:23:29 +0900

    inspector, java: remove needless fields
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- inspector/java/base/src/org/osrg/earthquake/HTTPEQHelper.java
+++ inspector/java/base/src/org/osrg/earthquake/HTTPEQHelper.java
@@ -6,7 +6,6 @@ import org.jboss.byteman.rule.helper.*;
 
 public class HTTPEQHelper extends EQHelper
 {
-    static org.osrg.earthquake.Inspector inspector;
     static {
     	inspector = new org.osrg.earthquake.HTTPInspector();
     };
--- inspector/java/base/src/org/osrg/earthquake/PBEQHelper.java
+++ inspector/java/base/src/org/osrg/earthquake/PBEQHelper.java
@@ -6,7 +6,6 @@ import org.jboss.byteman.rule.helper.*;
 
 public class PBEQHelper extends EQHelper
 {
-    static org.osrg.earthquake.Inspector inspector;
     static {
     	inspector = new org.osrg.earthquake.PBInspector();
     };
