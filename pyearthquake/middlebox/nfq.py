8cbc4703eae05512605b0cf2303a7e1916c6b15e
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2015-07-01 17:52:18 +0900

    [WIP] refine NFQ binding

--- pyearthquake/middlebox/nfq.py
+++ pyearthquake/middlebox/nfq.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python
-Q_NUM=42
-SOCK_BUF_SIZE=65536
-
-
-import struct
-import sys
-import time
 
 import socket
 from ctypes import *
-
-from scapy.all import *
-import hexdump as hd
+import prctl
 
 class NFQ(object):
     """
@@ -35,6 +26,18 @@ class NFQ(object):
                               c_void_p, #nfad
                               c_void_p) #data
 
+    def __init__(self, q_num, py_cb):
+        self._check_cap()
+        self.h = self._make_handle()
+        cb = self.callback_type(py_cb)
+        self.qh = self._make_queue_handle(self.h, q_num, cb)
+        self.fd = self.dll.nfq_fd(self.h)
+
+    @classmethod
+    def _check_cap(cls):
+        assert prctl.cap_permitted.net_admin, "missing CAP_NET_ADMIN"
+        assert prctl.cap_permitted.net_raw, "missing CAP_NET_RAW"
+        
     @classmethod
     def _make_handle(cls):
         h = cls.dll.nfq_open()
@@ -52,12 +55,6 @@ class NFQ(object):
         mode_set = cls.dll.nfq_set_mode(qh, cls.NFQNL_COPY_PACKET)
         assert mode_set >= 0, "mode_set=%d" % mode_set
         return qh
-
-    def __init__(self, q_num, py_cb):
-        self.h = self._make_handle()
-        cb = self.callback_type(py_cb)
-        self.qh = self._make_queue_handle(self.h, q_num, cb)
-        self.fd = self.dll.nfq_fd(self.h)
 
     def close(self):
         self.dll.nfq_close(self.h)
@@ -105,26 +102,32 @@ class NFQ(object):
         """
         return cls.dll.nfq_set_verdict(qh, packet_id, verdict, 0, 0)
 
-    
-def cb(qh, nfmsg, nfad, data):
-    """
-    int nfq_callback(struct nfq_q_handle *qh,
-		     struct nfgenmsg *nfmsg,
-		     struct nfq_data *nfad, void *data);
-    """
-    payload = NFQ.cb_get_payload(nfad)
-    packet_id = NFQ.cb_get_packet_id(nfad)
-    print "=== CB hexdump (packet id=%d) ===" % packet_id
-    hd.hexdump(payload)
-    ip = IP(payload)
-    print ip.summary()
-    NFQ.cb_set_verdict(qh, packet_id, NFQ.NF_ACCEPT)
-    return 1
 
-nfq = NFQ(Q_NUM, cb)
-s = socket.fromfd(nfq.fd, socket.AF_UNIX, socket.SOCK_STREAM)
-while True:
-        d = s.recv(SOCK_BUF_SIZE)
-        if d: nfq.handle_packet(d)
-s.close()
-nfq.close()
+if __name__ == "__main__":
+    Q_NUM=42
+    SOCK_BUF_SIZE=65536
+    from scapy.all import IP
+    from hexdump import hexdump
+
+    def cb(qh, nfmsg, nfad, data):
+        """
+        int nfq_callback(struct nfq_q_handle *qh,
+                         struct nfgenmsg *nfmsg,
+                         struct nfq_data *nfad, void *data);
+        """
+        payload = NFQ.cb_get_payload(nfad)
+        packet_id = NFQ.cb_get_packet_id(nfad)
+        hexdump(payload)
+        ip = IP(payload)
+        print "ID %d: %s" % (packet_id, ip.summary())
+        NFQ.cb_set_verdict(qh, packet_id, NFQ.NF_ACCEPT)
+        return 1
+
+    nfq = NFQ(Q_NUM, cb)
+    s = socket.fromfd(nfq.fd, socket.AF_UNIX, socket.SOCK_STREAM)
+    while True:
+            d = s.recv(SOCK_BUF_SIZE)
+            assert d
+            nfq.handle_packet(d)
+    s.close()
+    nfq.close()
