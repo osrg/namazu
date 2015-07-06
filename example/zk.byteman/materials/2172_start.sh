6101fd1cdc102de48144780eba9c3985e87c6cd2
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-07-06 18:09:29 +0900

    example, zk2172: refine test script
    save logs of preparation process
    
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- example/zk.byteman/materials/2172_start.sh
+++ example/zk.byteman/materials/2172_start.sh
@@ -5,18 +5,18 @@ function RECONFIG_ADD_SERVER(){
     trials=$2
     sleep_secs=$3
     for f in $(seq 1 ${trials}); do
-        INFO "Reconfig (sid=${sid}, trial=${f} of ${trials})"
+        echo "Reconfig (sid=${sid}, trial=${f} of ${trials})"
         tmp=$(mktemp)
         cmd="$MATERIALS_DIR/bin/zkCli.sh -server localhost:2181 reconfig -add server.${sid}=localhost:$((2887+sid)):$((3887+sid)):participant;$((2180+sid))"
-        INFO "Reconfig Invoking: ${cmd}"
+        echo "Reconfig Invoking: ${cmd}"
         ${cmd} 2>&1 | tee ${tmp}
         errors=$(grep KeeperErrorCode ${tmp} | wc -l)
         if [ $errors -eq 0 ]; then
             INFO "Reconfig success (sid=${sid})"; rm -f ${tmp}; return $(true)
         fi
-        INFO "Reconfig fail (sid=${sid}, trial=${f} of ${trials})"
+        echo "Reconfig fail (sid=${sid}, trial=${f} of ${trials})"
         rm -f $tmp
-        SLEEP ${sleep_secs}
+        sleep ${sleep_secs}
     done
     return $(false)
 }
@@ -37,6 +37,25 @@ do
     sed "s#dataDir=#dataDir=$DIR#" $CFG > $TMPCFG
     mv $TMPCFG $CFG
 done
+
+# store some data at first
+
+ZOO_LOG_DIR=$WORKING_DIR/zookeeper1/prepare_logs/1/ $MATERIALS_DIR/bin/zkServer.sh --config $WORKING_DIR/quorumconf/1 start
+ZOO_LOG_DIR=$WORKING_DIR/zookeeper2/prepare_logs/2/ $MATERIALS_DIR/bin/zkServer.sh --config $WORKING_DIR/quorumconf/2 start
+RECONFIG_ADD_SERVER 2 10 5
+
+for i in `seq 0 9`; do
+    $MATERIALS_DIR/bin/zkCli.sh -server localhost:2181 create /hoo-$i bar
+done
+
+for i in `seq 1 2`;
+do
+    $MATERIALS_DIR/bin/zkServer.sh --config $WORKING_DIR/quorumconf/$i stop
+done
+
+rm -rf $WORKING_DIR/zookeeper2/version-2/
+
+# run actual test
 
 EQ_MODE_DIRECT=1 EQ_ENV_PROCESS_ID=zksrv1 EQ_NO_INITIATION=1 SERVER_JVMFLAGS="-javaagent:$AGENT_CP=script:$MATERIALS_DIR/server.btm" ZOO_LOG_DIR=$WORKING_DIR/zookeeper1/logs/1/ $MATERIALS_DIR/bin/zkServer.sh --config $WORKING_DIR/quorumconf/1 start
 
