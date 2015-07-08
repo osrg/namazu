495c0fd9c185f1914f161393a9c238970e34603e
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-07-08 14:13:44 +0900

    orchestrator: rename process to transitionEntity
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- libearthquake/equtils/common.go
+++ libearthquake/equtils/common.go
@@ -55,3 +55,4 @@ type Event struct {
 type SingleTrace struct {
 	EventSequence []Event
 }
+
--- libearthquake/orchestrator.go
+++ libearthquake/orchestrator.go
@@ -26,13 +26,9 @@ import (
 	. "./searchpolicy"
 )
 
-type process struct {
-	id  string
-	idx int
-
-	machineID string // only for non direct mode
-
-	conn  net.Conn
+type transitionEntity struct {
+	id   string
+	conn net.Conn
 
 	reqToMain chan *InspectorMsgReq
 	gotoNext  chan interface{}
@@ -41,38 +37,37 @@ type process struct {
 	eventRspSend chan *InspectorMsgRsp
 }
 
-func handleProcessNoInitiation(proc *process, readyProcCh chan *process) {
-	go func(p *process) {
+func handleProcessNoInitiation(proc *transitionEntity, readyEntityCh chan *transitionEntity) {
+	go func(p *transitionEntity) {
 		for {
 			req := &InspectorMsgReq{}
 
 			rerr := RecvMsg(p.conn, req)
 			if rerr != nil {
 				if rerr == io.EOF {
-					Log("received EOF from process idx :%d", p.idx)
-					Log("recv routine end (process index :%d)", p.idx)
+					Log("received EOF from transition entity :%s", p.id)
 					return
 				} else {
-					Log("failed to recieve request (process index: %d): %s", p.idx, rerr)
+					Log("failed to recieve request (transition entity: %s): %s", p.id, rerr)
 					return // TODO: error handling
 				}
 			}
 
-			Log("received message from process idx :%d", p.idx)
+			Log("received message from transition entity :%s", p.id)
 			p.eventReqRecv <- req
 		}
 	}(proc)
 
-	go func(p *process) {
+	go func(p *transitionEntity) {
 		for {
 			rsp := <-p.eventRspSend
 			serr := SendMsg(p.conn, rsp)
 			if serr != nil {
-				Log("failed to send response (process index: %d): %s", p.idx, serr)
+				Log("failed to send response (transition entity: %s): %s", p.id, serr)
 				return // TODO: error handling
 			}
 			if *rsp.Res == InspectorMsgRsp_END {
-				Log("send routine end (process index :%d)", p.idx)
+				Log("send routine end (transition entity :%s)", p.id)
 				return
 			}
 		}
@@ -85,7 +80,7 @@ func handleProcessNoInitiation(proc *process, readyProcCh chan *process) {
 	go func() {
 		for {
 			req = <-proc.eventReqRecv
-			Log("received event from main goroutine: %v", proc)
+			Log("received event from main goroutine: %s", proc.id)
 			recvCh <- true
 		}
 	}()
@@ -94,22 +89,22 @@ func handleProcessNoInitiation(proc *process, readyProcCh chan *process) {
 		select {
 		case <-recvCh:
 			if *req.Type != InspectorMsgReq_EVENT {
-				Log("invalid message from process %v, type: %d", proc, *req.Type)
+				Log("invalid message from transition entity %s, type: %d", proc.id, *req.Type)
 				os.Exit(1)
 			}
 
-			if proc.id == "" {
+			if proc.id == "uninitialized" {
 				// initialize id with a member of event
 				proc.id = *req.ProcessId
 			}
 
-			Log("event message received from process %v", proc)
+			Log("event message received from transition entity %s", proc.id)
 
 			go func(r *InspectorMsgReq) {
 				proc.reqToMain <- r
 			}(req)
 
-			readyProcCh <- proc
+			readyEntityCh <- proc
 			if *req.Event.Type != InspectorMsgReq_Event_EXIT {
 				<-proc.gotoNext
 
@@ -127,7 +122,7 @@ func handleProcessNoInitiation(proc *process, readyProcCh chan *process) {
 	}
 }
 
-func acceptNewProcess(readyProcCh chan *process) {
+func acceptNewProcess(readyEntityCh chan *transitionEntity) {
 	sport := fmt.Sprintf(":%d", 10000) // FIXME
 	ln, lerr := net.Listen("tcp", sport)
 	if lerr != nil {
@@ -144,51 +139,51 @@ func acceptNewProcess(readyProcCh chan *process) {
 
 		Log("accepted new connection: %v", conn)
 
-		proc := new(process)
-		proc.id = ""
+		proc := new(transitionEntity)
+		proc.id = "uninitialized"
 		proc.conn = conn
 		proc.gotoNext = make(chan interface{})
 		proc.eventReqRecv = make(chan *InspectorMsgReq)
 		proc.eventRspSend = make(chan *InspectorMsgRsp)
 		proc.reqToMain = make(chan *InspectorMsgReq)
 
-		go handleProcessNoInitiation(proc, readyProcCh)
+		go handleProcessNoInitiation(proc, readyEntityCh)
 	}
 }
 
 func singleSearchNoInitiation(workingDir string, endCh chan interface{}, policy SearchPolicy, newTraceCh chan *SingleTrace) {
-	readyProcCh := make(chan *process)
+	readyEntityCh := make(chan *transitionEntity)
 
-	go acceptNewProcess(readyProcCh)
+	go acceptNewProcess(readyEntityCh)
 
 	eventSeq := make([]Event, 0)
 
 	nextEventChan := policy.GetNextEventChan()
-	ev2Proc := make(map[*Event]*process)
+	ev2entity := make(map[*Event]*transitionEntity)
 
 	running := true
 	for {
 		select {
-		case readyProc := <-readyProcCh:
+		case readyEntity := <-readyEntityCh:
 			if !running {
 				// run script ended, just ignore events
-				readyProc.gotoNext <- true
+				readyEntity.gotoNext <- true
 				continue
 			}
 
-			Log("ready process %v", readyProc)
-			req := <-readyProc.reqToMain
+			Log("ready process %v", readyEntity)
+			req := <-readyEntity.reqToMain
 			eventReq := req.Event
-			Log("recieved message from %v", readyProc)
+			Log("recieved message from %v", readyEntity)
 
 			if *eventReq.Type == InspectorMsgReq_Event_EXIT {
-				Log("process %v is exiting", readyProc)
+				Log("process %v is exiting", readyEntity)
 				continue
 			}
 
 			e := &Event{
 				ArrivedTime: time.Now(),
-				ProcId:      readyProc.id,
+				ProcId:      readyEntity.id,
 
 				EventType:  "FuncCall",
 				EventParam: *eventReq.FuncCall.Name,
@@ -225,15 +220,15 @@ func singleSearchNoInitiation(workingDir string, endCh chan interface{}, policy 
 				e.JavaSpecific = &ejs
 			}
 
-			ev2Proc[e] = readyProc
-			policy.QueueNextEvent(readyProc.id, e)
+			ev2entity[e] = readyEntity
+			policy.QueueNextEvent(readyEntity.id, e)
 
 		case nextEvent := <-nextEventChan:
-			readyProc := ev2Proc[nextEvent]
-			delete(ev2Proc, nextEvent)
+			readyEntity := ev2entity[nextEvent]
+			delete(ev2entity, nextEvent)
 
 			eventSeq = append(eventSeq, *nextEvent)
-			readyProc.gotoNext <- true
+			readyEntity.gotoNext <- true
 
 		case <-endCh:
 			Log("main loop end")
