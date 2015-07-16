5e9b579b6d9a47202259bd5a7a13617834091978
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-07-16 15:25:05 +0900

    historystorage, naive: handle a case of searched history is shorter than query
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- libearthquake/historystorage/naive/naive.go
+++ libearthquake/historystorage/naive/naive.go
@@ -222,7 +222,7 @@ func (n *Naive) GetRequiredTime(id int) (time.Duration, error) {
 func (n *Naive) Search(prefix []Event) []int {
 	// FIXME: quite ineffective
 
-	len := len(prefix)
+	prefixLen := len(prefix)
 	matched := make([]int, 0)
 
 	for i := 0; i < n.info.NrCollectedTraces-1; i++ { // FIXME: need to - 1 because the latest trace isn't recorded yet
@@ -232,7 +232,11 @@ func (n *Naive) Search(prefix []Event) []int {
 			os.Exit(1)
 		}
 
-		if AreEventsSliceEqual(prefix, history.EventSequence[:len]) {
+		if len(history.EventSequence) < prefixLen {
+			continue
+		}
+
+		if AreEventsSliceEqual(prefix, history.EventSequence[:prefixLen]) {
 			matched = append(matched, i)
 		}
 	}
