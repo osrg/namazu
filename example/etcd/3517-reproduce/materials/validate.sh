53a93def55fe786cf2bfb198a13bcb8924aff3f2
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2015-11-18 17:46:35 +0900

    example/etcd: fix

--- example/etcd/3517-reproduce/materials/validate.sh
+++ example/etcd/3517-reproduce/materials/validate.sh
@@ -3,7 +3,7 @@
 
 export ETCDCTL_PEERS=http://192.168.42.1:4001,http://192.168.42.2:4001,http://192.168.42.3:4001
 echo "getting key"
-$EQ_MATERIALS_DIR/etcd_testbed/etcd/bin/etcdctl --no-sync --timeout 10s get /k
+$EQ_MATERIALS_DIR/etcd_testbed/etcd/bin/etcdctl --no-sync --timeout 10s get /k1
 result=$?
 echo "result: $result"
 
