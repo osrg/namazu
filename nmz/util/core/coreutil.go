8447a1a754a56a03502bec63d4a83ec1bd4b1d2a
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2016-05-20 18:56:23 +0900

    *: Set version strings to v0.2.1-SNAPSHOT

--- nmz/util/core/coreutil.go
+++ nmz/util/core/coreutil.go
@@ -26,7 +26,7 @@ import (
 	logutil "github.com/osrg/namazu/nmz/util/log"
 )
 
-const NamazuVersion = "0.2.0"
+const NamazuVersion = "0.2.1-SNAPSHOT"
 
 // Returns true if NMZ_DEBUG is set
 func DebugMode() bool {
